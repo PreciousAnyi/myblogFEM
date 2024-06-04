c79a02746180a174ebf2d7249dfee6a86ec87c55
PreciousAnyi <jkpresh@gmail.com>
2024-06-04 18:12:00 +0100

    modified files

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,44 @@
+import styles from "../app/blog-preview-card-main/blog-preview-card-main.module.css";
 import Image from "next/image";
-import styles from "@/app/ui/page.module.css";
 
-export default function Home() {
+export default function Page() {
+  console.log(styles.page);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <body className={styles.page}>
+      <div className={styles.container}>
+        <div className={styles.shape}>
+          <div className={styles.image}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/images/blogimage.png"
+              alt="blog image"
+              width={336}
+              height={200}
             />
-          </a>
+          </div>
+          <div className={styles.container2}>
+            <div className={styles.learning}>
+              <p className={styles.bold_text2}>Learning</p>
+            </div>
+            <p className={styles.semibold_text}>Published 21 Dec 2023</p>
+            <h2 className={styles.bold_text}>HTML & CSS foundations</h2>
+            <p className={`${styles.semibold_text} ${styles.description}`}>
+              These languages are the backbone of every website, defining
+              structure, content, and presentation.
+            </p>
+            <div className={styles.author}>
+              <Image
+                src="/images/userpic.png"
+                alt="userpic"
+                width={32}
+                height={32}
+              />
+              <p className={`${styles.author_name} ${styles.bold_text}`}>
+                Greg Hooper
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </body>
   );
 }
